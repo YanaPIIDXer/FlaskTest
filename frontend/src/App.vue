c7d87bedcdc3e266f5bcdb003c3037b870410b66
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-07-05 22:32:19 +0900

    バックエンドを叩く処理を実装

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit="onSubmit">
+      <input type="text" v-model="message" />
+      <input type="submit" value="送信" />
+    </form>
+    <button @click="fetchMessage">メッセージ取得</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: function () {
+    return {
+      message: "Hello, World",
+    }
+  },
+  methods: {
+    /**
+     * 送信
+     */
+    onSubmit: async function (e) {
+      e.preventDefault()
+
+      try {
+        const response = await axios.post("http://localhost:3000/message", {
+          message: this.message,
+        })
+        if (!response || !response.data.result) { throw response }
+      } catch (error) {
+        console.error(error)
+        alert("エラー")
+        return
+      }
+
+      alert("送信しました") 
+      this.message = ""
+    },
+    /**
+     * メッセージ取得
+     */
+    fetchMessage: async function () {
+      let msg = ""
+      try {
+        const response = await axios.get("http://localhost:3000")
+        if (!response) { throw response }
+        msg = response.data.message
+      } catch (error) {
+        console.error(error)
+        alert("エラー")
+        return
+      }
+      alert(msg)
+    }
+  },
 }
 </script>
 
