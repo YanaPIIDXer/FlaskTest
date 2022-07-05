f46f26afdc16bcb862a0079e006d2d28a394449d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-07-05 21:58:23 +0900

    とりあえずHello, World

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def main():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return "Hello, World"
+
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    main()
